9a7894e5f9e256809f7b72f6cba2c5b0fa92e674
Martin <lapetajemartin@gmail.com>
2022-06-02 17:06:59 +0800

    added wrapper for direct logins

--- chat_app/lib/src/app.dart
+++ chat_app/lib/src/app.dart
@@ -1,7 +1,6 @@
-import 'package:chat_app/src/screens/authentication/login_screen.dart';
 import 'package:chat_app/src/service_locators.dart';
+import 'package:chat_app/wrapper.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 import 'controllers/navigation/navigation_service.dart';
 
@@ -13,31 +12,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // SystemChrome.setSystemUIOverlayStyle(
-    //   SystemUiOverlayStyle(
-    //     statusBarColor: Theme.of(context).primaryColorDark,
-    //   ),
-    // );
     return MaterialApp(
       restorationScopeId: 'app',
       debugShowCheckedModeBanner: false,
-      // ignore: prefer_const_constructors
-      // appBarTheme: AppBarTheme(
-      //   // systemOverlayStyle: SystemUiOverlayStyle.light, // 2
-      // ),
-      //     colorScheme: ColorScheme(
-      //   primary: Colors.red,
-      //   onPrimary: Colors.red,
-      //   secondary: Colors.red,
-      //   onSecondary: Colors.red,
-      //   error: Colors.red,
-      //   onError: Colors.red,
-      //   background: Colors.red,
-      //   onBackground: Colors.red,
-      //   surface: Colors.red,
-      //   onSurface: Colors.red,
-      //   brightness: Brightness.light,
-      // )
       theme: ThemeData(
           colorScheme: const ColorScheme.light().copyWith(
         primary: const Color(0xff6C63FF),
@@ -53,7 +30,7 @@ class MyApp extends StatelessWidget {
       builder: (context, Widget? child) => child as Widget,
       navigatorKey: locator<NavigationService>().navigatorKey,
       onGenerateRoute: NavigationService.generateRoute,
-      initialRoute: LoginScreen.route,
+      initialRoute: Wrapper.route,
     );
   }
 }
--- chat_app/lib/src/controllers/navigation/navigation_routing.dart
+++ chat_app/lib/src/controllers/navigation/navigation_routing.dart
@@ -2,9 +2,16 @@ part of 'navigation_service.dart';
 
 PageRoute getRoute(RouteSettings settings) {
   switch (settings.name) {
+    case Wrapper.route:
+      print("1Hellooooooooooooooooooooooooooooooooooo");
+
+      return FadeRoute(page: Wrapper(), settings: settings);
     case LoginScreen.route:
+      print("2Hellooooooooooooooooooooooooooooooooooo");
       return FadeRoute(page: const LoginScreen(), settings: settings);
     case HomeScreen.route:
+      print("3Hellooooooooooooooooooooooooooooooooooo");
+
       return FadeRoute(page: const HomeScreen(), settings: settings);
     default:
       return MaterialPageRoute(builder: (context) => const LoginScreen());
